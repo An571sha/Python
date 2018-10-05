388adaf268908d33bf9c98a6e3ca1dfc25bf3411
An571sha <anim_esh@yahoo.co.in>
2018-10-05 16:58:43 +0200

    1."guess" muss in der Schleife sein ansonsten fällt es in einer endlose Schleife 2. while not

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,11 +1,11 @@
 from random import randint
 answer = randint(1,10)
-guess = int(input("Guess a number between 1 and 10:"))
 number = False
-while(number):
+while not(number):
+    guess = int(input("Guess a number between 1 and 10:"))
     if(guess<answer):
         print("go higher")
-    elif(answer>guess) :
+    elif(answer>guess):
         print("go lower")
     else:
         print("correct")
