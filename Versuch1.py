85f3767f08720ef054949e2a473367774f293531
An571sha <anim_esh@yahoo.co.in>
2018-10-06 16:02:33 +0200

    no message

--- Versuch1.py
+++ Versuch1.py
@@ -5,4 +5,12 @@ Created on Sat Oct  6 15:19:48 2018
 
 @author: animesh
 """
-
+""" Mittelwert und standardabweichung berchnen 
+"""
+import numpy as np
+import csv
+filename = "Messung1.txt"
+reader = csv.reader(open("Messung1.txt","rb" ), delimiter=",")
+for row in reader:
+    print(row)
+    
