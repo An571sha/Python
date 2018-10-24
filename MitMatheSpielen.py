7cb30734151bec701c9d746f9614da5f0a44e7d6
An571sha <anim_esh@yahoo.co.in>
2018-10-24 12:22:53 +0200

    changed the two classes and added the methods mean and

--- MitMatheSpielen.py
+++ MitMatheSpielen.py
@@ -8,22 +8,25 @@ Created on Thu Oct 11 14:08:16 2018
 import numpy as np;
 from numpy import random;
 
-
+#künstlice Messdaten erzeugen
 def adata(xw,N,alpha,R):
     admat = np.zeros((N,R))
     
     for j in range(R):
-        admat[:,j] = xw + alpha*(random(0,N)-1);
+        admat[:,j] = xw+alpha*(random.rand(N)-1);
         
     return admat;
 
-R = 4;
-xw = 20;
-N = 1000;
+R = 1;
+xw = 2;
+N = 10;
 alpha = 0.5
 
 admat = adata(xw,N,alpha,R);
 
+#Mittelwert
 xM = sum(admat)/N;
-
+#Standardabweichung
 standardabweichung = np.sqrt(sum((admat-xM)**2)/(N-1));
+print(admat);
+print('s:',standardabweichung)
