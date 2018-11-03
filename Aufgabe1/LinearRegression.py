ea4c9af1904d31bf7991aaae4f3a63ebeaec56f4
An571sha <anim_esh@yahoo.co.in>
2018-11-03 09:56:12 +0100

    corrected the values and and the resulting graph

--- Aufgabe1/LinearRegression.py
+++ Aufgabe1/LinearRegression.py
@@ -14,7 +14,7 @@ arrayDis = [10,11,12,13,15,17,19,22,24,27,30,32,36,40,43,50,55,60,66,70]
 arrayMittelWert = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 i=0
 
-#Die Daten aus CSV Datei holen---------------------------------------------------------------------------------------------
+#Die Daten aus CSV Datei holen-------------------------------------------------------------------------------------------------
 
 for filename in glob.glob(os.path.join("/Users/animesh/Desktop/Python/Aufgabe1/abgabe1", "*.csv")):
     Spannung = np.genfromtxt(filename , delimiter= ",", skip_header=1000, usecols = (4))
@@ -22,15 +22,14 @@ for filename in glob.glob(os.path.join("/Users/animesh/Desktop/Python/Aufgabe1/a
     mittelwert = sum(Spannung)/NS
     arrayMittelWert[i]= mittelwert
     i = i+1
-#Logartimieren und plotten--------------------------------------------------------------------------------------------------------
+#Logartimieren----------------------------------------------------------------------------------------------------------------
 
 logSpannung = np.log(arrayMittelWert)
 logdist = np.log(arrayDis) 
     
 print(logSpannung)
 print(logdist)
-p.plot(logdist,logSpannung)
-p.show()    
+   
 
 
 meanLogSpannung = np.mean(logSpannung)
@@ -48,14 +47,14 @@ for i in range(len(logSpannung)):
     a2+=pow((logSpannung[i]-meanLogSpannung),2)                    #sum(x-Mx)^2
 
 a = a1/a2                     
-b = meanLogDist - a*meanLogSpannung                                #b = y - a*x
+b = meanLogDist - a*meanLogSpannung 
+p.figure(1) 
+p.plot(logdist,[a * x for x in logdist])
 
 print("a:",a)
 print("b:",b)    
 
-#x = arrayDis
-#y = (np.exp(a*x+b) for x in logSpannung)
-#p.plot(x,y)
-#p.show()
-fig, ax = p.subplots()
-ax.plot([np.exp(a * x + b) for x in logSpannung], arrayDis)
+p.figure(2)
+p.plot( arrayDis,[np.exp(a * x + b) for x in logdist])
+
+p.show()
